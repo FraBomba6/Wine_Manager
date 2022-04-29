3821f7e1a05d60a82062f0bd37a8d983e5d7ed68
bomb697 <50354770+bomb697@users.noreply.github.com>
2022-04-29 14:16:46 +0200

    Building the page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 let knex = require("knex")({
     client: "sqlite3",
@@ -8,32 +8,34 @@ let knex = require("knex")({
     }
 });
 
-const createWindow = () => {
+function createWindow() {
     const win = new BrowserWindow({
         autoHideMenuBar: true,
-        width: 800,
-        height: 600,
+        show: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
-    win.loadFile('index.html')
-    return win
+    win.loadFile('index.html').then(() => {
+        win.maximize();
+        win.show();
+    })
+    return win;
 }
 
 app.whenReady().then(() => {
-    const win = createWindow()
+    const win = createWindow();
     ipcMain.on("mainWindowLoaded", () => {
         let result = knex("vini").where({
             "cantina": "Tommasi"
         }).select("nome")
         result.then(function(rows){
             win.webContents.send("resultSent", rows);
-        })
-    })
-})
+        });
+    });
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') app.quit();
+});
